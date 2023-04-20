c1cfba6343340c1e37070e77d5b040da9de2d576
vndato <vinhdattruong1309@gmail.com>
2023-04-20 14:17:36 +0200

    Added Main and GamePanel class, creates window with Thread

---
+++ RPG/src/main/GamePanel.java
@@ -0,0 +1,40 @@
+package RPG.src.main;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 Tiles
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        
+    }
+
+}
